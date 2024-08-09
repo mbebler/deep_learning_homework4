2e2c67eed342b2b4b0bb441b4daf8a2d3e7d9105
madel <mbebler@utexas.edu>
2024-08-08 21:06:24 -0500

    8-8 - version 1 of convolution

--- homework/train_planner.py
+++ homework/train_planner.py
@@ -25,49 +25,49 @@ Recall that a training pipeline includes:
 
 def train(
         exp_dir: str = "logs",
-        num_epoch: int = 21,
+        num_epoch: int = 51,
         lr: float = 1e-3,
         batch_size: int = 32,
         seed: int = 2024,
 ):
-    # set random seed so each run is deterministic
-    torch.manual_seed(seed)
-    np.random.seed(seed)
+  # set random seed so each run is deterministic
+  torch.manual_seed(seed)
+  np.random.seed(seed)
 
-    # directory with timestamp to save tensorboard logs and model checkpoints
-    log_dir = Path(exp_dir) / f"{datetime.now().strftime('%m%d_%H%M%S')}"
-    print(f"{datetime.now().strftime('%m%d_%H%M%S')}")
-    logger = tb.SummaryWriter(log_dir)
+  # directory with timestamp to save tensorboard logs and model checkpoints
+  log_dir = Path(exp_dir) / f"{datetime.now().strftime('%m%d_%H%M%S')}"
+  print(f"{datetime.now().strftime('%m%d_%H%M%S')}")
+  logger = tb.SummaryWriter(log_dir)
 
-    # loading the data
-    train_data = load_data("deep_learning_homework4/road_data/train", shuffle=True,
+  # loading the data
+  train_data = load_data("deep_learning_homework4/road_data/train", shuffle=True,
                            batch_size=batch_size)
-    val_data = load_data("deep_learning_homework4/road_data/val", shuffle=False)
+  val_data = load_data("deep_learning_homework4/road_data/val", shuffle=False)
 
-    # load the model
-    #if vers:
+  # load the model
+   #if vers:
         #net = MLPPlanner()
         #print("MLP")
     #else:
-    net = TransformerPlanner()
-    print("Transformer")
-    net.cuda()
-    net.train()
+  net = TransformerPlanner()
+  print("Transformer")
+  net.cuda()
+  net.train()
 
     # create the loss function and optimizer
-    loss_func = torch.nn.MSELoss()
-    optim = torch.optim.SGD(net.parameters(), lr=lr)
-    train_metrics = PlannerMetric()
-    val_metrics = PlannerMetric()
-    global_step = 0
+  loss_func = torch.nn.MSELoss()
+  optim = torch.optim.AdamW(net.parameters(), lr=lr)
+  train_metrics = PlannerMetric()
+  val_metrics = PlannerMetric()
+  global_step = 0
 
     # now to train the model
-    for epoch in range(num_epoch):
-      # print(epoch)
-      train_metrics.reset()
-      val_metrics.reset()
-      net.train()
-      for data in train_data:
+  for epoch in range(num_epoch):
+    # print(epoch)
+    train_metrics.reset()
+    val_metrics.reset()
+    net.train()
+    for data in train_data:
         # input the data
         left_bev = data["bev_track_left"].cuda()
         # print(left_bev.shape)
@@ -91,37 +91,37 @@ def train(
 
         global_step += 1
 
-      # now we add the overall accuracy to the logger
-      train_metrics2 = train_metrics.compute()
-      logger.add_scalar("train/accuracy-long", np.mean(train_metrics2["longitudinal_error"]), global_step=epoch)
-      logger.add_scalar("train/accuracy-lat", np.mean(train_metrics2['lateral_error']), global_step=epoch)
+        # now we add the overall accuracy to the logger
+        train_metrics2 = train_metrics.compute()
+        logger.add_scalar("train/accuracy-long", np.mean(train_metrics2["longitudinal_error"]), global_step=epoch)
+        logger.add_scalar("train/accuracy-lat", np.mean(train_metrics2['lateral_error']), global_step=epoch)
 
-      # now we want to look at the validation data
-      net.eval()
-      val_accuracy = []
-      for data in val_data:
-        # input the data
-        left_bev = data["bev_track_left"].cuda()
-        right_bev = data["bev_track_right"].cuda()
-        way = data["waypoints"].cuda()
-        way_mask = data["waypoints_mask"].cuda()
-        # find the model
-        with torch.inference_mode():
-          output = net(bev_track_left = left_bev, bev_track_right = right_bev)
-          # print((net.predict(x = data) == label).float().mean())
-          val_metrics.add(output, way, way_mask)
+    # now we want to look at the validation data
+    net.eval()
+    val_accuracy = []
+    for data in val_data:
+    # input the data
+      left_bev = data["bev_track_left"].cuda()
+      right_bev = data["bev_track_right"].cuda()
+      way = data["waypoints"].cuda()
+      way_mask = data["waypoints_mask"].cuda()
+      # find the model
+      with torch.inference_mode():
+        output = net(bev_track_left = left_bev, bev_track_right = right_bev)
+        # print((net.predict(x = data) == label).float().mean())
+        val_metrics.add(output, way, way_mask)
 
 
-      #log the mean val accuracy in logger
-      val_metrics2 = val_metrics.compute()
-      logger.add_scalar("val/accuracy-long", np.mean(val_metrics2["longitudinal_error"]), global_step=epoch)
-      logger.add_scalar("val/accuracy-lat", np.mean(val_metrics2['lateral_error']), global_step=epoch)
-      print(np.mean(val_metrics2['lateral_error']), np.mean(val_metrics2["longitudinal_error"]))
+    #log the mean val accuracy in logger
+    val_metrics2 = val_metrics.compute()
+    logger.add_scalar("val/accuracy-long", np.mean(val_metrics2["longitudinal_error"]), global_step=epoch)
+    logger.add_scalar("val/accuracy-lat", np.mean(val_metrics2['lateral_error']), global_step=epoch)
+    print(np.mean(val_metrics2['lateral_error']), np.mean(val_metrics2["longitudinal_error"]))
 
-      logger.flush()
+    logger.flush()
 
-        # now we save models every so often
-      if epoch % 10 == 0:
-        print(epoch)
-        save_model(net)
-        print(calculate_model_size_mb(net))
+    # now we save models every so often
+    if epoch % 10 == 0:
+      print(epoch)
+      print(calculate_model_size_mb(net))
+      save_model(net)
